809f5bca84d3edf6fbbaf7e2b3a2ae90642a913e
wangping <wangping003@lianlianpay.com>
2022-11-23 11:31:54 +0800

    xx

--- mylib/sign.py
+++ mylib/sign.py
@@ -1,4 +1,13 @@
 from Crypto.Hash import MD5
 
 def my_hash111(data):
-    return MD5.new(data.encode('utf-8'))
+    return MD5.new(data.encode('utf-8')).hexdigest()
+
+
+def read_file(filename):
+    with open(filename, 'r',encoding='utf-8') as f:
+        for line in f:
+            print(line)
+
+
+
